9798fbb526e256ee88a289bbba5d66da8d19e693
Renan-Santana <renanblog15@gmail.com>
2021-08-30 22:52:55 -0300

    muito sono

--- Game/src/App.java
+++ Game/src/App.java
@@ -4,9 +4,9 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Alvo[] alvos = new Alvo[3];
+        Alvo[] alvos = new Alvo[15];
         criarAlvos(alvos);
-        //atirar(alvos);
+        // atirar(alvos);
         for (int i = 0; i < alvos.length; i++) {
             System.out.print(alvos[i].toString() + "\n");
         }
@@ -16,13 +16,15 @@ public class App {
     private static void criarAlvos(Alvo[] alvos) {
         Random random = new Random();
         for (int i = 0; i < alvos.length; i++) {
-            int x, y;
-            do {
+            int x = random.nextInt(5) + 1;
+            int y = random.nextInt(5) + 1;
+            alvos[i] = new Alvo(x, y, 'B');
+            while (verificarProsicao(alvos, x, y)) {
                 x = random.nextInt(5) + 1;
                 y = random.nextInt(5) + 1;
                 alvos[i].setPosx(x);
                 alvos[i].setPosy(y);
-            } while (verificarProsicao(alvos[i], x, y));
+            }
 
         }
     }
@@ -31,10 +33,15 @@ public class App {
 
     }
 
-    private static boolean verificarProsicao(Alvo alvo, int x, int y) {
-        if (alvo.getPosx() == x && alvo.getPosy() == y) {
-            return false;
+    private static boolean verificarProsicao(Alvo[] alvos, int x, int y) {
+        for (int i = 0; i < alvos.length; i++) {
+            if (alvos[i] == null) {
+                return true;
+            } else if (alvos[i].getPosx() == x && alvos[i].getPosy() == y) {
+                return false;
+            }
         }
+
         return true;
     }
 
