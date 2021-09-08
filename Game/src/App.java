f175f262cc51307fc7b65290cb2c818b92244329
RENAN TEIXEIRA CARDOZO SANTANA <renanblog15@gmail.com>
2021-09-08 19:12:26 -0300

    Codigo rodrigo

--- Game/src/App.java
+++ Game/src/App.java
@@ -12,7 +12,6 @@ public class App {
         for (Alvo alvo : alvos) {
             System.err.println(alvo.toString());
         }
-
         start(alvos);
 
     }
@@ -63,6 +62,21 @@ public class App {
         }
     }
 
+    // classe criada: getCor(Alvo tiro, Arraylist<Alvo> alvos) - pode ser a classe
+    // atira
+
+    private static char Atira(int tiroX, int tiroY, ArrayList<Alvo> alvos) {
+        Alvo tiro = new Alvo(tiroX, tiroY, ' ');
+        for (Alvo alvo : alvos) {
+            if (alvo.getPosx() == tiroX && alvo.getPosy() == tiroY) {
+                tiro.setCor(alvo.getCor());
+            }
+        }
+        return tiro.getCor();
+    }
+
+    // FIM
+
     private static boolean pesquisaAlvo(int x, int y, ArrayList<Alvo> alvos) {
         for (Alvo alvo : alvos) {
             if (alvo.getPosx() == x && alvo.getPosy() == y) {
@@ -76,45 +90,117 @@ public class App {
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
         int qtdTiros = 10;
-        while (qtdTiros != 0 && Alvo.getVida() != 0) {
+        while (Alvo.getVida() > 0) {
             System.out.print("Insira a posicao 'X' dos eu tiro(1-5): ");
             int tiroX = scan.nextInt();
 
             System.out.print("Insira a posicao 'Y' dos eu tiro(1-5): ");
             int tiroY = scan.nextInt();
-
-            for (Alvo alvo : alvos) {
-                if (alvo.getPosx() == tiroX && alvo.getPosy() == tiroY) {
-                    if (alvo.Atira(tiroX, tiroY) == 'P') {
-                        System.err.println("Acertou um alvo preto");
-                        alvos.remove(alvo);
-                        int x = random.nextInt(5) + 1;
-                        int y = random.nextInt(5) + 1;
-                        while (verificarPosicoes(x, y, alvos)) {
-                            x = random.nextInt(5) + 1;
-                            y = random.nextInt(5) + 1;
-                        }
-                        Alvo novoAlvo = new Alvo(x, y, 'P');
-                        alvos.add(novoAlvo);
-
-                        qtdTiros--;
-                    } else if (alvo.Atira(tiroX, tiroY) == 'B') {
-                        System.err.println("Acertou um alvo branco");
-                        alvos.remove(alvo);
-                        qtdTiros--;
-                    } else {
-                        System.err.println("Erroou!");
-                        qtdTiros--;
-                    }
+            char cor = ' ';
+            Alvo tiro = new Alvo(tiroX, tiroY, cor);
+            if (Atira(tiroX, tiroY, alvos) == 'P') {
+                System.err.println("Acertou um alvo preto");
+                alvos.remove(tiro);
+                int x = random.nextInt(5) + 1;
+                int y = random.nextInt(5) + 1;
+                while (verificarPosicoes(x, y, alvos)) {
+                    x = random.nextInt(5) + 1;
+                    y = random.nextInt(5) + 1;
+                    Alvo.vida--;
                 }
+                Alvo novoAlvo = new Alvo(x, y, 'P');
+                alvos.add(novoAlvo);
+                qtdTiros--;
+            } else if (Atira(tiroX, tiroY, alvos) == 'B') {
+                System.err.println("Acertou um alvo branco");
+                alvos.remove(tiro);
+                Alvo.vida++;
+                qtdTiros--;
+            } else {
+                System.err.println("Erroou!");
+                qtdTiros--;
+            }
+            if (qtdTiros == 0) {
+                System.out.println("GAME OVER");
+            } else if (Alvo.getVida() == 0) {
+                System.out.println("VENCEU");
             }
         }
 
-        if (qtdTiros == 0) {
-            System.out.println("GAME OVER!");
-        } else {
+        if (Alvo.getVida() == 0 && qtdTiros != 0) {
             System.out.println("VENCEU!");
+        } else if (qtdTiros == 0) {
+            System.out.println("GAME OVER");
         }
+        System.out.print(Alvo.getVida());
     }
 
 }
+
+/*--------------------------------------------------------------------------------------------------*/
+
+/*
+ * import java.util.Scanner; import java.util.ArrayList; import
+ * java.util.Random;
+ * 
+ * public class App { public static void main(String[] args) throws Exception {
+ * 
+ * ArrayList<Alvo> alvos = new ArrayList<Alvo>(15);
+ * 
+ * criarAlvos(alvos);
+ * 
+ * for (Alvo alvo : alvos) { System.err.println(alvo.toString()); }
+ * 
+ * start(alvos);
+ * 
+ * }
+ * 
+ * private static void criarAlvos(ArrayList<Alvo> alvos) { Random random = new
+ * Random(); Alvo alvo; int cont = 15; while (cont != 0) { int x =
+ * random.nextInt(5) + 1; int y = random.nextInt(5) + 1; if
+ * (verificarPosicoes(x, y, alvos)) { if (cont <= 3) { alvo = new Alvo(x, y,
+ * 'B'); alvos.add(alvo); cont--; } else { alvo = new Alvo(x, y, 'P');
+ * alvos.add(alvo); cont--; }
+ * 
+ * } } }
+ * 
+ * private static boolean verificarPosicoes(int x, int y, ArrayList<Alvo> alvos)
+ * { for (Alvo alvo : alvos) { if (alvo.getPosx() == x && alvo.getPosy() == y) {
+ * return false; } } return true; }
+ * 
+ * private static void criaAlvoPreto(ArrayList<Alvo> alvos) { Random random =
+ * new Random(); Alvo alvo; int cont = alvos.size(); while (cont != 0) { int x =
+ * random.nextInt(5) + 1; int y = random.nextInt(5) + 1; if
+ * (verificarPosicoes(x, y, alvos)) { alvo = new Alvo(x, y, 'P');
+ * alvos.add(alvo); cont--; } } }
+ * 
+ * private static boolean pesquisaAlvo(int x, int y, ArrayList<Alvo> alvos) {
+ * for (Alvo alvo : alvos) { if (alvo.getPosx() == x && alvo.getPosy() == y) {
+ * return true; } } return false; }
+ * 
+ * private static void start(ArrayList<Alvo> alvos) { Random random = new
+ * Random(); Scanner scan = new Scanner(System.in); int qtdTiros = 10; while
+ * (qtdTiros != 0 && Alvo.getVida() != 0) {
+ * System.out.print("Insira a posicao 'X' dos eu tiro(1-5): "); int tiroX =
+ * scan.nextInt();
+ * 
+ * System.out.print("Insira a posicao 'Y' dos eu tiro(1-5): "); int tiroY =
+ * scan.nextInt();
+ * 
+ * for (Alvo alvo : alvos) { if (alvo.getPosx() == tiroX && alvo.getPosy() ==
+ * tiroY) { if (alvo.Atira(tiroX, tiroY) == 'P') {
+ * System.err.println("Acertou um alvo preto"); alvos.remove(alvo); int x =
+ * random.nextInt(5) + 1; int y = random.nextInt(5) + 1; while
+ * (verificarPosicoes(x, y, alvos)) { x = random.nextInt(5) + 1; y =
+ * random.nextInt(5) + 1; } Alvo novoAlvo = new Alvo(x, y, 'P');
+ * alvos.add(novoAlvo);
+ * 
+ * qtdTiros--; } else if (alvo.Atira(tiroX, tiroY) == 'B') {
+ * System.err.println("Acertou um alvo branco"); alvos.remove(alvo); qtdTiros--;
+ * } else { System.err.println("Erroou!"); qtdTiros--; } } } }
+ * 
+ * if (qtdTiros == 0) { System.out.println("GAME OVER!"); } else {
+ * System.out.println("VENCEU!"); } }
+ * 
+ * }
+ */
