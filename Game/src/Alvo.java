7df493cd203480a9d17c707fc669c3801774e6f2
RENAN TEIXEIRA CARDOZO SANTANA <renanblog15@gmail.com>
2021-09-08 20:53:01 -0300

    Finish

--- Game/src/Alvo.java
+++ Game/src/Alvo.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+/* import java.util.ArrayList;
 
 public class Alvo {
 
@@ -56,38 +56,66 @@ public class Alvo {
         return "Alvo [cor=" + cor + ", posx=" + posx + ", posy=" + posy + "]";
     }
 
-}
+} */
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-/*
- * public class Alvo {
- * 
- * private static int vida = 3; private int posx, posy; private char cor;
- * 
- * public Alvo(int posx, int posy, char cor) { this.posx = posx; this.posy =
- * posy; this.cor = cor; }
- * 
- * public static int getVida() { return vida; }
- * 
- * public int getPosx() { return posx; }
- * 
- * public void setPosx(int posx) { this.posx = posx; }
- * 
- * public int getPosy() { return posy; }
- * 
- * public void setPosy(int posy) { this.posy = posy; }
- * 
- * public char getCor() { return cor; }
- * 
- * public void setCor(char cor) { this.cor = cor; }
- * 
- * public char Atira(int posicaoX, int posicaoY) { if (posicaoX == this.posx &&
- * posicaoY == this.posy) { if (this.cor == 'B') { Alvo.vida++; return 'B'; }
- * else { Alvo.vida--; return 'P'; } } return 'N'; }
- * 
- * @Override public String toString() { return "Alvo [cor=" + cor + ", posx=" +
- * posx + ", posy=" + posy + "]"; }
- * 
- * }
- */
+public class Alvo {
+
+    private static int vida = 3;
+    private int posx, posy;
+    private char cor;
+
+    public Alvo(int posx, int posy, char cor) {
+        this.posx = posx;
+        this.posy = posy;
+        this.cor = cor;
+    }
+
+    public static int getVida() {
+        return vida;
+    }
+
+    public int getPosx() {
+        return posx;
+    }
+
+    public void setPosx(int posx) {
+        this.posx = posx;
+    }
+
+    public int getPosy() {
+        return posy;
+    }
+
+    public void setPosy(int posy) {
+        this.posy = posy;
+    }
+
+    public char getCor() {
+        return cor;
+    }
+
+    public void setCor(char cor) {
+        this.cor = cor;
+    }
+
+    public char Atira(int posicaoX, int posicaoY) {
+        if (posicaoX == this.posx && posicaoY == this.posy) {
+            if (this.cor == 'B') {
+                Alvo.vida++;
+                return 'B';
+            } else {
+                Alvo.vida--;
+                return 'P';
+            }
+        }
+        return 'N';
+    }
+
+    @Override
+    public String toString() {
+        return "Alvo [cor=" + cor + ", posx=" + posx + ", posy=" + posy + "]";
+    }
+
+}
